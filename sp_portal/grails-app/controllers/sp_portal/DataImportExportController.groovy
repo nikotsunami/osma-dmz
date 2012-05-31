f67a73f7ca136e6831db8c59b38ab12bf9b3db37
Marvin Sheng <marvin@bizpoint.cn>
2012-05-31 09:23:41 +0800

    create user on data import

--- sp_portal/grails-app/controllers/sp_portal/DataImportExportController.groovy
+++ sp_portal/grails-app/controllers/sp_portal/DataImportExportController.groovy
@@ -6,7 +6,8 @@ import org.codehaus.groovy.grails.web.json.*;
 
 class DataImportExportController extends MainController {
 
-    def beforeInterceptor = [action:this.&isLoggedInAsAdmin]
+    //def beforeInterceptor = [action:this.&isLoggedInAsAdmin]
+   
 
     //static allowedMethods = [save: "POST", update: "POST", delete: "POST"]
 
@@ -20,16 +21,37 @@ class DataImportExportController extends MainController {
                        "StandardizedPatient.anamnesisForm.anamnesisChecksValues.anamnesisCheck":{ id ->local.AnamnesisCheck.findByOrigId(id)},
                        "StandardizedPatient.anamnesisForm.scars":{ id ->local.Scar.findByOrigId(id)},
                        ]
-    static creators = [ "StandardizedPatient": { id -> def x = new local.StandardizedPatient(); x.origId = id; return x},
-                       "StandardizedPatient.bankaccount":{ id -> def x = new local.Bankaccount(); x.origId = id; return x},
-                       "StandardizedPatient.anamnesisForm":{ id -> def x = new local.AnamnesisForm(); x.origId = id; return x},
-                       "StandardizedPatient.description":{ id -> def x = new local.Description(); x.origId = id; return x},
-                       "StandardizedPatient.profession":{ id -> def x = new local.Profession(); x.origId = id; return x},
-                       "StandardizedPatient.nationality":{ id -> def x = new local.Nationality(); x.origId = id; return x},
-                       "StandardizedPatient.anamnesisForm.anamnesisChecksValues":{ id -> def x = new local.AnamnesisChecksValue(); x.origId = id; return x},
-                       "StandardizedPatient.anamnesisForm.anamnesisChecksValues.anamnesisCheck":{ id -> def x = new local.AnamnesisCheck(); x.origId = id; return x},
-                       "StandardizedPatient.anamnesisForm.scars":{ id -> def x = new local.Scar(); x.origId = id; return x},
+    static creators = [ "StandardizedPatient": { id,jsonData-> 
+    																					 				def x = new local.StandardizedPatient();
+    																					 				x.origId = id; 
+    																					 				x.save(); 
+    																					 				createUser(x,jsonData);
+    																					 				return x},
+                       "StandardizedPatient.bankaccount":{ id,jsonData -> def x = new local.Bankaccount(); x.origId = id; return x},
+                       "StandardizedPatient.anamnesisForm":{ id,jsonData -> def x = new local.AnamnesisForm(); x.origId = id; return x},
+                       "StandardizedPatient.description":{ id,jsonData -> def x = new local.Description(); x.origId = id; return x},
+                       "StandardizedPatient.profession":{ id,jsonData -> def x = new local.Profession(); x.origId = id; return x},
+                       "StandardizedPatient.nationality":{ id,jsonData -> def x = new local.Nationality(); x.origId = id; return x},
+                       "StandardizedPatient.anamnesisForm.anamnesisChecksValues":{ id,jsonData -> def x = new local.AnamnesisChecksValue(); x.origId = id; return x},
+                       "StandardizedPatient.anamnesisForm.anamnesisChecksValues.anamnesisCheck":{ id,jsonData -> def x = new local.AnamnesisCheck(); x.origId = id; return x},
+                       "StandardizedPatient.anamnesisForm.scars":{ id,jsonData -> def x = new local.Scar(); x.origId = id; return x},
                        ]
+   	static def createUser(standardizedPatient,jsonData){
+   		 def x =new User();
+   		 x.userName= jsonData.email;
+   		 x.passwordHash=encodePassword(""+jsonData.socialInsuranceNo,x.userName); 
+   		 x.userEmail=jsonData.email;
+   		 x.standardizedPatient=standardizedPatient;
+   		 x.isActive=true;
+   		 def roles = [];
+       roles.add(Role.findByRoleName("USER_ROLE"));
+       x.roles = roles;
+   		 x.save();
+   		 //return x;
+   	
+   	
+   	} 
+
 
     static exclusions = ["hasOne","validationSkipMap","gormPersistentEntity",
                         "PersistentEntity","properties","metaClass","class","id","dirty",
@@ -52,9 +74,6 @@ class DataImportExportController extends MainController {
     }
 
 
-// {"class":"sp_portal.local.StandardizedPatient","anamnesisForm":{"class":"sp_portal.local.AnamnesisForm","anamnesisChecksValues":[{"class":"sp_portal.local.AnamnesisChecksValue","anamnesisCheck":{"class":"sp_portal.local.AnamnesisCheck","id":3,"sortOrder":10,"text":"Nehmen Sie zurzeit regelmässig Medikamente ein?","title":{"class":"sp_portal.local.AnamnesisCheck","id":12,"sortOrder":9,"text":"Treatment history category","title":null,"type":4,"userSpecifiedOrder":null,"value":""},"type":1,"userSpecifiedOrder":null,"value":""},"anamnesisChecksValue":null,"anamnesisForm":{"_ref":"../..","class":"sp_portal.local.AnamnesisForm"},"comment":null,"id":9,"truth":false},{"class":"sp_portal.local.AnamnesisChecksValue","anamnesisCheck":{"class":"sp_portal.local.AnamnesisCheck","id":1,"sortOrder":2,"text":"Rauchen Sie?","title":{"class":"sp_portal.local.AnamnesisCheck","id":10,"sortOrder":1,"text":"Personal lifestyle category","title":null,"type":4,"userSpecifiedOrder":null,"value":""},"type":1,"userSpecifiedOrder":null,"value":""},"anamnesisChecksValue":null,"anamnesisForm":{"_ref":"../..","class":"sp_portal.local.AnamnesisForm"},"comment":null,"id":8,"truth":true}],"createDate":new Date(1279382400000),"id":3,"scars":[],"standardizedPatients":[{"_ref":"../..","class":"sp_portal.local.StandardizedPatient"}]},"bankaccount":{"class":"sp_portal.local.Bankaccount","bankName":"Basler Kantonalbank","bic":"GENODEF1JEV","city":null,"iban":"CH46 3948 4853 2029 3","id":20,"ownerName":null,"postalCode":null,"standardizedPatients":[{"_ref":"../..","class":"sp_portal.local.StandardizedPatient"}]},"birthday":new Date(-484560000000),"city":"Metz","description":null,"email":"paul 111 m.lamarie@unibas.ch","gender":1,"height":162,"immagePath":null,"maritalStatus":null,"mobile":"078 427 24 85","name":"Lamarie","nationality":{"class":"sp_portal.local.Nationality","nationality":"Frankreich","id":7},"id":20,"postalCode":4057,"preName":"Marianne","profession":{"class":"sp_portal.local.Profession","id":3,"profession":"Bauarbeiter/in"},"socialInsuranceNo":null,"street":"Feldbergstrasse 145","telephone":null,"telephone2":null,"videoPath":null,"weight":57,"workPermission":null}
-
-// {"class":"sp_portal.local.StandardizedPatient","anamnesisForm":{"class":"sp_portal.local.AnamnesisForm","anamnesisChecksValues":[{"class":"sp_portal.local.AnamnesisChecksValue","anamnesisCheck":{"class":"sp_portal.local.AnamnesisCheck","id":3,"sortOrder":10,"text":"Nehmen Sie zurzeit regelmässig Medikamente ein?","title":{"class":"sp_portal.local.AnamnesisCheck","id":12,"sortOrder":9,"text":"Treatment history category","title":null,"type":4,"userSpecifiedOrder":null,"value":""},"type":1,"userSpecifiedOrder":null,"value":""},"anamnesisChecksValue":null,"anamnesisForm":{"_ref":"../..","class":"sp_portal.local.AnamnesisForm"},"comment":null,"id":9,"truth":false},{"class":"sp_portal.local.AnamnesisChecksValue","anamnesisCheck":{"class":"sp_portal.local.AnamnesisCheck","id":1,"sortOrder":2,"text":"Rauchen Sie?","title":{"class":"sp_portal.local.AnamnesisCheck","id":10,"sortOrder":1,"text":"Personal lifestyle category","title":null,"type":4,"userSpecifiedOrder":null,"value":""},"type":1,"userSpecifiedOrder":null,"value":""},"anamnesisChecksValue":null,"anamnesisForm":{"_ref":"../..","class":"sp_portal.local.AnamnesisForm"},"comment":null,"id":8,"truth":true}],"createDate":new Date(1279382400000),"id":3,"scars":[],"standardizedPatients":[{"_ref":"../..","class":"sp_portal.local.StandardizedPatient"}]},"bankaccount":{"class":"sp_portal.local.Bankaccount","bankName":"Basler Kantonalbank","bic":"GENODEF1JEV","city":null,"iban":"CH46 3948 4853 2029 3","id":20,"ownerName":null,"postalCode":null,"standardizedPatients":[{"_ref":"../..","class":"sp_portal.local.StandardizedPatient"}]},"birthday":new Date(-484560000000),"city":"Metz","description":null,"email":"paul 111 m.lamarie@unibas.ch","gender":1,"height":162,"immagePath":null,"maritalStatus":null,"mobile":"078 427 24 85","name":"Lamarie","nationality":{"class":"sp_portal.local.Nationality","nationality":"Frankreich","id":7},"id":20,"postalCode":4057,"preName":"Marianne","profession":{"class":"sp_portal.local.Profession","id":3,"profession":"Bauarbeiter/in"},"socialInsuranceNo":null,"street":"Feldbergstrasse 145","telephone":null,"telephone2":null,"videoPath":"hello nick","weight":57,"workPermission":null}
 
 /*
 
@@ -69,15 +88,16 @@ class DataImportExportController extends MainController {
                                                             "comment":null,"id":9,"truth":false},
                                                  {"class":"sp_portal.local.AnamnesisChecksValue",
                                                      "anamnesisCheck":{"class":"sp_portal.local.AnamnesisCheck","id":1,"sortOrder":2,"text":"Rauchen Sie?",
-                                                            "title":{"class":"sp_portal.local.AnamnesisCheck","id":10,"sortOrder":1,"text":"Personal lifestyle category","title":null,"type":4,"userSpecifiedOrder":null,"value":""},
+                                                            "title":{"class":"sp_portal.local.AnamnesisCheck","id":101,"sortOrder":1,"text":"Personal lifestyle category","title":null,"type":4,"userSpecifiedOrder":null,"value":""},
                                                             "type":1,"userSpecifiedOrder":null,"value":""},
                                                             "anamnesisChecksValue":null,"anamnesisForm":{"_ref":"../..","class":"sp_portal.local.AnamnesisForm"},"comment":null,"id":8,"truth":true},
                                                  {"class":"sp_portal.local.AnamnesisChecksValue",
                                                      "anamnesisCheck":{"class":"sp_portal.local.AnamnesisCheck","id":1,"sortOrder":2,"text":"Rauchen Sie 222?",
-                                                            "title":{"class":"sp_portal.local.AnamnesisCheck","id":10,"sortOrder":1,"text":"Personal lifestyle category","title":null,"type":4,"userSpecifiedOrder":null,"value":""},
+                                                            "title":{"class":"sp_portal.local.AnamnesisCheck","id":101,"sortOrder":1,"text":"Personal lifestyle category","title":null,"type":4,"userSpecifiedOrder":null,"value":""},
                                                             "type":1,"userSpecifiedOrder":null,"value":"JDJDJDDJDJDJ"},
                                                             "anamnesisChecksValue":null,"anamnesisForm":{"_ref":"../..","class":"sp_portal.local.AnamnesisForm"},"comment":null,"id":80,"truth":true}
                                                 ],
+                                                
 
         "createDate":new Date(1279382400000),
         "id":3,
@@ -89,14 +109,14 @@ class DataImportExportController extends MainController {
                                                 "bic":"GENODEF1JEV",
                                                 "city":null,
                                                 "iban":"CH46 3948 4853 2029 3",
-                                                "id":200,
+                                                "id":2000,
                                                 "ownerName":null,
                                                 "postalCode":null,
                                                 "standardizedPatients":[{"_ref":"../..","class":"sp_portal.local.StandardizedPatient"}]},
         "birthday":new Date(-484560000000),
         "city":"Metz",
         "description":null,
-        "email":"paul 111 m.lamarie@unibas.ch",
+        "email":"qqq@rrr.com",
         "gender":1,
         "height":162,
         "immagePath":null,
@@ -105,14 +125,14 @@ class DataImportExportController extends MainController {
         "name":"Lamarie",
         "nationality":{"class":"sp_portal.local.Nationality",
                         "nationality":"Frankreich","id":7},
-        "id":20,
+        "id":570,
         "postalCode":4057,
         "preName":"Marianne",
         "profession":{"class":"sp_portal.local.Profession","id":3,"profession":"Bauarbeiter/in"},
-        "socialInsuranceNo":null,
+        "socialInsuranceNo":"qqq",
         "street":"Feldbergstrasse 145",
         "telephone":null,
-        "telephone2":null,
+        "telephone2":";kjlkjlj",
         "videoPath":"hello nick",
         "weight":57,
         "workPermission":null}
@@ -123,12 +143,12 @@ class DataImportExportController extends MainController {
 
 // "birthday":new Date(-484560000000)
 // "createDate":new Date(1279382400000)
-
+		
 
     def importSP(){
- println( "+++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++");
+ 
         if (params.data){
-            def jsonObject = JSON.parse(params.data);
+             def jsonObject = JSON.parse(params.data);
             syncData(new JSONObject(jsonObject));
             render jsonObject.email;
         } else {
@@ -147,14 +167,14 @@ class DataImportExportController extends MainController {
     }
 
 
-
+  
+    
 
 
     private def syncOneClass(jsonObject, datapath ){
         println( "syncOneClass  " + datapath);
 
         if (!jsonObject || (jsonObject == JSONObject.NULL)  || !jsonObject.id){
-            println( "1 No JSON data for field  " + datapath);
             return false;
         }
 
@@ -163,7 +183,8 @@ class DataImportExportController extends MainController {
 
         if (!sp) {
             if (creators[datapath]){
-                sp = creators[datapath](jsonObject.id);
+                sp = creators[datapath](jsonObject.id, jsonObject);
+              
             } else {
                 return false;
             }
@@ -173,10 +194,11 @@ class DataImportExportController extends MainController {
         // loop over all the proerties in the class
         sp.metaPropertyValues.each{ prop ->
 
-         //   println("3) -----------property: " + prop.name + "-----" + prop.type)
 
             // avoid the read only properties
             if(exclusions.find {it == prop.name}) return
+            
+         
 
 
             // if it is a basic type
@@ -187,7 +209,6 @@ class DataImportExportController extends MainController {
 
                     // set the value to the one from JSON
                     if ( jsonObject[prop.name] && (jsonObject[prop.name] != JSONObject.NULL)){
-                        println( "4)  Set property " + prop.name + " with type " + prop.type + " to  " + jsonObject[prop.name] + " of type" + jsonObject[prop.name].class )
                         sp[prop.name] = jsonObject.get(prop.name);
                     } else {
 
@@ -209,15 +230,13 @@ class DataImportExportController extends MainController {
             } else {
 
                // not a basic type
-               println( "7) Field  " + prop.name + " " + prop.type );
                if (Date != prop.type){
 
                    def fieldFinder = finders[datapath+"."+prop.name];
                    if (fieldFinder){
                         // known Entity relationship
                         if (prop.type != Set){
-                            println("//  1 to 1 relationship");
-
+														//  1 to 1 relationship
                             // Are the ids the same?
                             if ((jsonObject[prop.name] != JSONObject.NULL) && jsonObject[prop.name].id ){
                                 // Yes good so sync the fields
@@ -227,13 +246,13 @@ class DataImportExportController extends MainController {
                                   def newValue = syncOneClass(jsonObject[prop.name] ,datapath+"."+prop.name );
                                   if (newValue){
                                         sp[prop.name] = newValue;
+                                        // do we need to delete the original // maybe cause difficult problems.
                                   }
                             }
 
 
 
                         } else {
-                            println("~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~// one to many relationship");
                             //  one to many relationship, so clear the current values and reinitialize from JSON data
                             sp[prop.name] = []
 
@@ -245,7 +264,6 @@ class DataImportExportController extends MainController {
 
                                       // No then overwrite
                                       def newValue = syncOneClass(member,datapath+"."+prop.name );
-                                       println("===============================================================");
                                        if (newValue){
                                             println("aDDING  " + datapath+"         "+newValue );
                                             sp[prop.name].add(newValue);
@@ -273,12 +291,9 @@ class DataImportExportController extends MainController {
 
             }
 
-
         }    // end loop over all the proerties in the class
 
-        println(".............................................................");
         sp.save();
-        println(".............................................................");
         return sp;
 
     }
