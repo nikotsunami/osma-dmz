43a035b13b372181fae9708564ead8de3cf002eb
Elaine Qi <elaine@bizpoint.cn>
2012-07-03 17:11:52 +0800

    bug 652

--- sp_portal/grails-app/conf/BootStrap.groovy
+++ sp_portal/grails-app/conf/BootStrap.groovy
@@ -1,7 +1,17 @@
+import sp_portal.Role
+import sp_portal.User
 class BootStrap {
 
     def init = { servletContext ->
+	    if (Role.list().size() == 0){
+                def adminRole = new Role(id:1,roleName:"ADMIN_ROLE",roleDescription:"Administrate Users");
+                adminRole.save();
+
+                def userRole = new Role(id:2,roleName:"USER_ROLE",roleDescription:"Normal Users");
+                userRole.save();
+            
+
+           }
     }
-    def destroy = {
-    }
+    def destroy = { }
 }
