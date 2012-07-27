08d025c6bcc1ad91117be1126277975e0b92cdd4
Paul Smout <paul@bizpoint.cn>
2012-07-27 10:15:44 +0800

    confirmed additional field in JSON sync have no effect

--- sp_portal/test/integration/sp_portal/DataImportExportControllerTests.groovy
+++ sp_portal/test/integration/sp_portal/DataImportExportControllerTests.groovy
@@ -379,6 +379,7 @@ class DataImportExportControllerTests extends GroovyTestCase{
         def ret = $/
                 {
                        "class":"ch.unibas.medizin.osce.domain.StandardizedPatient",
+                       "cats":"dogs", // this is an unexpected field 
                        "anamnesisForm":{
                           "class":"ch.unibas.medizin.osce.domain.AnamnesisForm",
                           "anamnesisChecksValues":[
@@ -591,6 +592,7 @@ class DataImportExportControllerTests extends GroovyTestCase{
                                 "class":"ch.unibas.medizin.osce.domain.AnamnesisChecksValue",
                                 "anamnesisCheck":{
                                    "class":"ch.unibas.medizin.osce.domain.AnamnesisCheck",
+                                   "cats":"dogs", // this is an unexpected field 
                                    "id":2,
                                    "sortOrder":2,
                                    "text":"Rauchen Sie 222?",
